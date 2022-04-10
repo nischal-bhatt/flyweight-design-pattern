3c578c44f580f5426807758b1da226d790c077e7
nischal <nischal.bhatt.87@gmail.com>
2022-04-10 12:34:30 +0800

    committing flyweight design pattern

---
+++ src/com/coffeepoweredcrew/flyweight/SystemErrorMessage.java
@@ -0,0 +1,28 @@
+package com.coffeepoweredcrew.flyweight;
+
+//A concrete Flyweight. Instance is shared
+public class SystemErrorMessage implements ErrorMessage{
+
+	
+	
+	//some error message $errorCode --> 
+	private String messageTemplate;
+	
+	//https://somedomain.com/help?error=
+	private String helpUrlBase;
+	
+	
+	public SystemErrorMessage(String messageTemplate, String helpUrlBase)
+	{
+		this.messageTemplate = messageTemplate;
+		this.helpUrlBase = helpUrlBase;
+	}
+	
+	@Override
+	public String getText(String code) {
+		
+		return messageTemplate.replace("$errorCode", code) + helpUrlBase + code;
+	}
+
+
+}
